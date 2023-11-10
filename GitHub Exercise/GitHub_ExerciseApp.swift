9812ccd35c18c4cb9ecf7a4a71ae105f0a03fa2f
Dagmar Feldt <d.feldt@t-online.de>
2023-11-10 18:02:39 +0100

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Dagmar Feldt on 10.11.23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
